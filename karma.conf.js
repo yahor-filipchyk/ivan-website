51d78e2b850e41a62f5b3f8103baecdebc68afc2
Y.Filipchyk <yahor.filipchyk@vegafactor.com>
2017-07-23 03:11:02 -0400

    Use singleRun on Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+var ON_CI_SERVER = process.env.TEST_SUITE;
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -39,6 +41,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: !!ON_CI_SERVER
   });
 };
